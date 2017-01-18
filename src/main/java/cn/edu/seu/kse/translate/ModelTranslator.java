624e311843670e2b3330e505dde048c1ac74bf1f
张舒韬 <1527740142@qq.com>
2017-01-18 15:09:11 +0800

    feat(翻译)：语法单元的通用翻译接口设计
    Signed-off-by: 张舒韬 <1527740142@qq.com>

---
+++ src/main/java/cn/edu/seu/kse/translate/ModelTranslator.java
@@ -0,0 +1,25 @@
+package cn.edu.seu.kse.translate;
+
+import cn.edu.seu.kse.model.ObjectModel;
+
+import java.util.Set;
+
+/**
+ * 翻译器的通用接口
+ * Created by 张舒韬 on 2017/1/17.
+ */
+public interface ModelTranslator {
+    /**
+     * 翻译任意语法单元
+     * @param objectModel 待翻译的语法单元
+     * @return 翻译所得的语法单元集合
+     */
+    Set<ObjectModel> translate(ObjectModel objectModel);
+
+    /**
+     * 将逻辑程序翻译成另一形式的逻辑程序
+     * @param program 待翻译的逻辑程序
+     * @return 翻译所得的逻辑程序
+     */
+    ObjectModel translateProgram(ObjectModel program);
+}
